8ae76b53506b8f0d11463e7c2492dad416850150
ahmedsatti101 <ahmedysatti@gmail.com>
2024-01-15 20:04:05 +0000

    Completed task four

--- controllers/topic.controller.js
+++ controllers/topic.controller.js
@@ -1,5 +1,9 @@
-const { retrieveTopics, addTopic } = require("../models/topic.model");
-const endpointsFile = require('../endpoints.json');
+const {
+  retrieveTopics,
+  retrieveArticleById,
+} = require("../models/topic.model");
+const endpointsFile = require("../endpoints.json");
+const fs = require("fs/promises");
 
 exports.getAllTopics = (req, res) => {
   retrieveTopics().then((topics) => {
@@ -7,16 +11,27 @@ exports.getAllTopics = (req, res) => {
   });
 };
 
-exports.postTopic = (req, res, next) => {
-    const newTopic = req.body;
-
-    addTopic(newTopic).then((topic) => {
-        res.status(201).send({topic});
-    }).catch((err) => {
-        next(err);
-    })
-}
-
 exports.getAllEndpoints = (req, res) => {
-    res.status(200).send(endpointsFile)
-}
+  res.status(200).send(endpointsFile);
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  retrieveArticleById(article_id)
+  .then((article) => {
+      res.status(200).send({ article });
+      fs.readFile("./endpoints.json", "utf-8").then((endpoint) => {
+        const parsedData = JSON.parse(endpoint);
+        parsedData['GET /api/articles:article_id'] = {
+            description: "get an article by its id",
+            queries: [],
+            exampleResponse: [{article}]
+        };
+        return fs
+          .writeFile("./endpoints.json", JSON.stringify(parsedData, null, 2))
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
