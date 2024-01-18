0bbfd6e5558e024d91066cfabdc68372398a1129
ahmedsatti101 <ahmedysatti@gmail.com>
2024-01-18 10:41:02 +0000

    get-api-articles-article_id-comment_count

--- models/topic.model.js
+++ models/topic.model.js
@@ -7,17 +7,28 @@ exports.retrieveTopics = () => {
 };
 
 exports.retrieveArticleById = (article_id) => {
-  return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: "Article not found",
-        });
-      }
-      return result.rows[0];
-    });
+  const requestedArticleId = [];
+  let queryStr = `SELECT articles.*, COUNT(comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments
+  ON comments.article_id = articles.article_id`;
+
+  if (article_id) {
+    requestedArticleId.push(article_id);
+    queryStr += ` WHERE articles.article_id = $1`;
+  }
+
+  queryStr += ` GROUP BY articles.article_id`;
+
+  return db.query(queryStr, requestedArticleId).then((result) => {
+    if (result.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "Article not found",
+      });
+    }
+    return result.rows;
+  });
 };
 
 exports.retrieveArticles = (topic) => {
